b3258a9460c030327eececa49a93fb36344645ec
Jessica Mehaffey <jessmehaffey@fulsail.edu>
2012-11-01 16:51:53 -0400

    Added comments to guide coding process.

--- javascript/main.js
+++ javascript/main.js
@@ -2,3 +2,24 @@
 // Term: VFW 1211
 // For: Project 2
 // Date: 11/1/2012
+
+// Define Global Variables
+
+
+// Create Local Storage
+// Stores input information on user's computer
+
+
+// Clear Form Link
+// Erases all input fields
+
+
+// Save Button
+// Sends input fields into local storage
+// Should be able to be accessed 
+
+
+// Access Local Storage
+// Link this section to the "Display Registered Pets" link.
+// Should clear form, and display all pets added.
+// createElement - display info to user
