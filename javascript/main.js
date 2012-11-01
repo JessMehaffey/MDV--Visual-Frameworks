c08b48afe4300ac7886c82bc8a843605837c740b
Jessica Mehaffey <jessmehaffey@fulsail.edu>
2012-11-01 17:41:09 -0400

    Added var links and addEventListener for display/clear/save.

--- javascript/main.js
+++ javascript/main.js
@@ -3,23 +3,41 @@
 // For: Project 2
 // Date: 11/1/2012
 
-// Define Global Variables
+// Make sure HTML is loaded before running Javascript
+window.addEventListener("DOMContentLoaded", function(){
+
+	// getElementById Function
+	function $(x){
+		var theElement = document.getElementById(x);
+		return theElement;
+	}
+
+	// Variable defaults
+	var petSpecies = ["--Choose Species--", "Dog", "Cat", "Fish", "Bird", "Horse", "Reptile", "Rodent", "Other"];
+	
+	
+	// Display Link
+	var displayPets = $("displayPets");
+	displayPets.addEventListener("click", getPetData);
+	
+	// Clear Link
+	var clearForm = $("clearForm");
+	clearForm.addEventListener("click", clearData);
+	
+	// Save Button
+	var savePage = $("submit");
+	savePage.addEventListener("click", saveData);
+	
+	
+
+	// Access Local Storage
+	// Link this section to the "Display Registered Pets" link.
+	// Should clear form, and display all pets added.
+	// createElement - display info to user
 
 
-// Create Local Storage
-// Stores input information on user's computer
 
 
-// Clear Form Link
-// Erases all input fields
 
-
-// Save Button
-// Sends input fields into local storage
-// Should be able to be accessed 
-
-
-// Access Local Storage
-// Link this section to the "Display Registered Pets" link.
-// Should clear form, and display all pets added.
-// createElement - display info to user
+// Ending brackets for window.AddEventListener
+});
