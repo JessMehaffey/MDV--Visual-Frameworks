854ba19a92df9058b95806d4763670b518c1ced0
Jessica Mehaffey <jessmehaffey@fulsail.edu>
2012-11-15 13:49:23 -0500

    Added var json and started to define.

--- javascript/json.js
+++ javascript/json.js
@@ -3,3 +3,23 @@
 // For: Project 4
 // Date: 11/15/2012
 
+var json = {
+	"pet1": {
+		"petName": ["Pet Name:", "Poco"],
+		"group": [],
+		"gender": [],
+		"temperamentSlider": [],
+		"morningFed": [],
+		"eveningFed": [],
+		"watered": [],
+		"medicated": [],
+		"walked": [],
+		"bathed": [],
+		"cleanedCage": [],
+		"ranAway": [],
+		"accident": [],
+		"date": [],
+		"extraNotes": [],
+	}
+
+}
